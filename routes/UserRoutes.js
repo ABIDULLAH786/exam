c80cecb9f91210c35ac82469fca57e4e131b3db4
ABIDULLAH786 <abidullah.bsef18@iba-suk.edu.pk>
2022-05-24 16:30:39 +0500

    all work done

--- routes/UserRoutes.js
+++ routes/UserRoutes.js
@@ -1,11 +1,13 @@
 const express = require("express")
 const router = express.Router();
-const {displayRegPage,addNewUser,userTable} = require("../controller/UserController")
+const {displayRegPage,addNewUser,userTable,deletById,findUserById} = require("../controller/UserController")
 // auth/:method
 // router.route("/auth/:method").get(signInPage);
 router.route("/").get(displayRegPage);
 router.route("/new").post(addNewUser);
 router.route("/users").get(userTable);
+router.route("/user/:id").get(findUserById);
+router.route("/delete/:id").get(deletById);
 
 
 
